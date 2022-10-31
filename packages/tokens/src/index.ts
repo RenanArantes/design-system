be1e80a9c8c6259f43730398d0bb29d5daa4019e
RenanArantes <malevo18@gmail.com>
2022-10-31 14:11:41 -0300

    Configuração do ESLint

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,4 +1,4 @@
-//o arquivo index server para diminuir o caminho da exportação quando a lib for chamada
+// o arquivo index server para diminuir o caminho da exportação quando a lib for chamada
 
 export * from './colors'
 
