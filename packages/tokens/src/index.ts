95af8140d2db0c44246809e6eb21533107e9f40f
RenanArantes <malevo18@gmail.com>
2022-10-31 10:14:09 -0300

    Configurando tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,3 +1,11 @@
 //o arquivo index server para diminuir o caminho da exportação quando a lib for chamada
 
 export * from './colors'
+
+export * from './space'
+export * from './radii'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
